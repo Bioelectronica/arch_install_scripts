7ee7ea761bfe4d8f37b7dd4de5b8b833ef35f2fd
Roger Chen <roger@bioelectronica.com>
2020-03-29 11:31:31 -0700

    sda option

--- 02.sh
+++ 02.sh
@@ -1,6 +1,6 @@
-S1="$1"
-S2="p2"
-TARGET="$S1$S2"
+#S1="$1"
+#S2="p2"
+#TARGET="$S1$S2"
 bootctl --path=/boot install 
 cat - > /boot/loader/loader.conf << EOF
 default arch
@@ -10,7 +10,7 @@ title Bioelectronica
 linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
-options root=$TARGET rw quiet
+options root=$1 rw quiet
 EOF
 systemctl enable sshd
 systemctl enable NetworkManager
