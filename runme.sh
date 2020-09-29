8ecc200fec6380edba622cf8b2a25d6a3f6bbf82
Roger Chen <roger@bioelectronica.com>
2020-09-29 10:58:19 -0700

    vnc

--- runme.sh
+++ runme.sh
@@ -11,8 +11,8 @@ yay -S rc-local --noconfirm
 #------------------------------------------------------------- GUI
 echo "exec openbox-session" > ~/.xinitrc
 cat /root/arch_install_scripts/bash_profile_addendum.sh >> ~/.bash_profile
-sudo pacman -S xorg xorg-xinit openbox ttf-dejavu ttf-liberation tint2 network-manager-applet --noconfirm
-sudo pacman -S tk pcmanfm gpicview mupdf tigervnc chromium guvcview --noconfirm
+sudo pacman -S xorg-server xorg-xinit xterm openbox ttf-dejavu ttf-liberation tint2 network-manager-applet --noconfirm
+sudo pacman -S tk pcmanfm gpicview mupdf tigervnc chromium guvcview light arandr --noconfirm
 sudo pacman -S imagemagick imagemagick-doc ghostscript libheif libraw libwmf --noconfirm
 sudo cp /root/arch_install_scripts/rc.xml /etc/xdg/openbox/
 sudo cp /root/arch_install_scripts/menu.xml /etc/xdg/openbox/
@@ -28,6 +28,11 @@ sudo pacman -S python-seaborn python-scikit-learn jupyter-notebook --noconfirm
 sudo pacman -S python-pyserial arduino-docs arduino-avr-core arduino --noconfirm
 sudo pacman -S python-pip python-google-api-python-client python-oauth2client ffmpeg --noconfirm
 yay -S python-bokeh --noconfirm
+yay -S python-pytest python-psutil
+yay -S arduino-cli
+#------------------------------------------------------------- configure
+arduino-cli core update-index
+arduino-cli core install arduino:avr
+sudo echo ":0=saveguest" >> /etc/tigervnc/vncserver.users
 #------------------------------------------------------------- problem stuff at end
 yay -S icdiff --noconfirm
-
