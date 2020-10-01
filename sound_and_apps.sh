05e7effc2f6814b78cf00283383e4a2dddaf83d2
Roger Chen <roger@bioelectronica.com>
2020-10-01 08:25:27 -0700

    app stuff

--- sound_and_apps.sh
+++ sound_and_apps.sh
@@ -3,9 +3,9 @@
 yay -S pulseaudio pulseaudio-alsa pulseaudio-bluetooth pulseaudio-equalizer pulseaudio-jack pulseaudio-zeroconf alsa-utils pavucontrol --noconfirm
 systemctl --user enable pulseaudio
 systemctl --user enable pulseaudio.socket
-yay -S gnome-disks-utility --noconfirm
+yay -S gnome-disk-utility --noconfirm
 yay -S google-chrome --noconfirm
 yay -S zoom --noconfirm
-yay -S audacity pinta shotwell libreoffice-still
+yay -S audacity pinta shotwell libreoffice-still --noconfirm
 cp /root/arch_install_scripts/dotconfig_tint2_tint2rc ~/.config/tint2/tint2rc
-
+sudo cp /root/arch_install_scripts/org.gnome.DiskUtility.desktop /usr/share/applications/ 
