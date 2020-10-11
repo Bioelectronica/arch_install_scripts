ff3829235ae621013c21bb2593f5d5fd9acfbf33
Roger Chen <roger@bioelectronica.com>
2020-10-11 12:12:33 -0700

    swap

--- make4gbswap.sh
+++ make4gbswap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 dd if=/dev/zero of=/swapfile bs=1M count=4096 status=progress
+mkswap /swapfile
 chmod 600 /swapfile
 swapon /swapfile
 echo "/swapfile none swap defaults 0 0" >> /etc/fstab
